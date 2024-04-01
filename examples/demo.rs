75251832666424fd06f5d6cf0b9146612f8b18d6
marc2332 <mespinsanz@gmail.com>
2024-04-01 23:43:34 +0200

    clean up

--- examples/demo.rs
+++ examples/demo.rs
@@ -2,7 +2,7 @@ use dioxus::prelude::*;
 use dioxus_library_template::prelude::*;
 
 fn main() {
-    dioxus_desktop::launch(app);
+    launch(app);
 }
 
 fn app() -> Element {
--- src/hooks/use_dioxus.rs
+++ src/hooks/use_dioxus.rs
@@ -1,5 +1,3 @@
-use dioxus::core::ScopeState;
-
 /// Get the Dioxus description.
 pub fn use_dioxus() -> &'static str {
     "Dioxus is a Rust library for building apps that run on desktop, web, mobile, and more."
